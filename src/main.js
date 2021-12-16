b3d073444c0ca39eb14c05f9352fa274ce7439ab
pl1998 <2139714249@qq.com>
2021-12-16 17:44:56 +0800

    修复一些bug

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import Cookies from 'js-cookie'
 
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
-import 'font-awesome/css/font-awesome.min.css';
+import 'font-awesome/css/font-awesome.min.css'
 // import { createI18n } from 'vue-i18n'
 import Element from 'element-ui'
 import './styles/element-variables.scss'
 import enLang from 'element-ui/lib/locale/lang/en'// 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
 
 import '@/styles/index.scss' // global css
-
 
 import App from './App'
 import store from './store'
@@ -19,34 +18,32 @@ import './icons' // icon
 import './permission' // permission control
 import './utils/error-log' // error log
 
-//图标选择
-import iconPicker from 'e-icon-picker';
-import "e-icon-picker/dist/symbol.js"; //基本彩色图标库
-import 'e-icon-picker/dist/index.css'; // 基本样式，包含基本图标
-import 'font-awesome/css/font-awesome.min.css'; //font-awesome 图标库
-import 'element-ui/lib/theme-chalk/icon.css'; //element-ui 图标库
+// 图标选择
+import eIconPicker from 'e-icon-picker'
+import 'e-icon-picker/lib/symbol.js' // 基本彩色图标库
+import 'e-icon-picker/lib/index.css' // 基本样式，包含基本图标
+import 'font-awesome/css/font-awesome.min.css' // font-awesome 图标库
+import 'element-ui/lib/theme-chalk/icon.css' // element-ui 图标库
 
+Vue.use(eIconPicker, { FontAwesome: true, ElementUI: true, eIcon: true, eIconSymbol: true })
 
-Vue.use(iconPicker, {FontAwesome: true, ElementUI: true, eIcon: true, eIconSymbol: true});
-process.env.ENV == 'production' &&
-	Sentry.init({
-		dsn: 'https://912d97ea26704c828aff071141a4ca50@o408607.ingest.sentry.io/5299000',
-		integrations: [ new VueIntegration({ Vue, attachProps: true }) ]
-	})
+// process.env.ENV === 'production' &&
+// 	Sentry.init({
+// 	  dsn: 'https://912d97ea26704c828aff071141a4ca50@o408607.ingest.sentry.io/5299000',
+// 	  integrations: [new VueIntegration({ Vue, attachProps: true })]
+// 	})
 
-
-  Vue.use(Element, {
-	size: 'mini' // set element-ui default size
-	//locale: enLang // 如果使用中文，无需设置，请删除
+Vue.use(Element, {
+  size: 'mini' // set element-ui default size
+  // locale: enLang // 如果使用中文，无需设置，请删除
 })
 // Vue.use(createI18n)
 
 import * as filters from './filters' // global filters
 
-//引入图标库
+// 引入图标库
 Vue.prototype.base_host = process.env.VUE_APP_HOST + process.env.VUE_APP_BASE_API ? process.env.VUE_APP_BASE_API : 'api'
 Vue.prototype.host = process.env.VUE_APP_HOST
-
 
 /**
  * If you don't want to use mock-server
@@ -70,7 +67,6 @@ Vue.use(Element, {
 Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key])
 })
-
 
 Vue.config.productionTip = false
 
