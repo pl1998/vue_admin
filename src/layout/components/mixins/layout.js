3c9ef4264379f53ffcb3382501500a79f91cdc36
pl1998 <2139714249@qq.com>
2022-03-28 17:14:13 +0800

    fix:调整

--- src/layout/components/mixins/layout.js
+++ src/layout/components/mixins/layout.js
@@ -1,139 +1,156 @@
+import { mapState } from 'vuex'
 export default {
-	data() {
-		return {
-			total: 100,
-			listLoading: true,
-			listQuery: {
-				page: 1,
-				pageSize:2,
-				importance: undefined,
-				title: undefined,
-				type: undefined,
-				sort: '+id'
-			},
-			name: undefined,
-			content: '222',
-			dialogHtmlVisible: false,
-			header: undefined
-		}
-	},
-	methods: {
-		/**
+  computed: mapState({
+    users: (state) => state.user
+  }),
+  data() {
+    return {
+      total: 100,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        pageSize: 2,
+        importance: undefined,
+        title: undefined,
+        type: undefined,
+        sort: '+id'
+      },
+      name: undefined,
+      content: '222',
+      dialogHtmlVisible: false,
+      header: undefined
+    }
+  },
+  methods: {
+    // import {checkPermission} from '../../utils/check'
+
+    checkPermission(api, method) {
+      if (api) {
+        if (this.users.node === undefined) {
+          return true
+        }
+        const apis = this.users.node
+        const rest = apis.filter(function(item) {
+          return item[1] === api && (item[2] === method || item[2] === '*')
+        })
+        return !(rest.length > 0)
+      } else {
+        return false
+      }
+    },
+    /**
          * 是否确认关闭
          * @param {*} done
          */
-		handleClose(done) {
-			this.$confirm('确认关闭？')
-				.then((_) => {
-					done()
-				})
-				.catch((_) => {})
-		},
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then((_) => {
+          done()
+        })
+        .catch((_) => {})
+    },
 
-		/**
+    /**
          * 重置列表
          */
-		resetForm() {
-			this.filter = this.$options.data().filter
-			this.getList()
-		},
-
-
+    resetForm() {
+      this.filter = this.$options.data().filter
+      this.getList()
+    },
     /**
      * 清除空参数
      * @param {*} params
      */
-		filterEmpty(params) {
-			for (const key in params) {
-				let param = params[key]
-				let type = typeof param
-				let isEmpty = type == 'undefined' || type == null
-				if (isEmpty) {
-					delete params[key]
-				} else {
-					param.length == 0 && delete params[key]
-				}
-			}
-			return params
-		},
-
-	/**
+    filterEmpty(params) {
+      for (const key in params) {
+        const param = params[key]
+        const type = typeof param
+        const isEmpty = type === 'undefined' || type === null
+        if (isEmpty) {
+          delete params[key]
+        } else {
+          param.length === 0 && delete params[key]
+        }
+      }
+      return params
+    },
+    /**
    * 处理验证器
    * @param {*} formName
    */
-		validateForm(formName) {
-			return new Promise((resolve, reject) => {
-				this.$refs[formName].validate((valid) => {
-					if (valid) {
-						resolve(true)
-					} else {
-						reject()
-					}
-				})
-			})
-		},
+    validateForm(formName) {
+      return new Promise((resolve, reject) => {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            resolve(true)
+          } else {
+            reject()
+          }
+        })
+      })
+    },
 
-		/**
+    /**
          * 从编辑器中取出来前获取一下参数
          */
-		setInput(field) {
-			this.form[field] = this.$refs.editorForm.getHtml()
-		},
+    setInput(field) {
+      this.form[field] = this.$refs.editorForm.getHtml()
+    },
 
-		/**
+    /**
          * 查看富文本
          */
-		showHtml(name, content) {
-			this.name = name
-			this.content = content
-			this.dialogHtmlVisible = true
-		},
+    showHtml(name, content) {
+      this.name = name
+      this.content = content
+      this.dialogHtmlVisible = true
+    },
 
-		/**
+    /**
          * 二次确认框
          * @param {*} message
          */
-		confirmOk(message = '是否确认', title = '提示') {
-			return new Promise((resolve, reject) => {
-				this.$confirm(message, title, {
-					confirmButtonText: '确认',
-					cancelButtonText: '取消',
-					type: 'warning'
-				})
-					.then(() => {
-						resolve()
-					})
-					.catch(() => {
-						reject()
-					})
-			})
-		},
+    confirmOk(message = '是否确认', title = '提示') {
+      return new Promise((resolve, reject) => {
+        this.$confirm(message, title, {
+          confirmButtonText: '确认',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+          .then(() => {
+            resolve()
+          })
+          .catch(() => {
+            reject()
+          })
+      })
+    },
 
-		/**
+    /**
          * 提示
          * @param {*} message
          */
-		message(message = 'success', type = 'success', duration = 2000) {
-			this.$message({
-				type: type,
-				message: message,
-				duration: duration
-			})
-		},
+    message(message = 'success', type = 'success', duration = 2000) {
+      this.$message({
+        type: type,
+        message: message,
+        duration: duration
+      })
+    },
 
-		errMessage(message = 'error', type = 'error', duration = 2000) {
-			this.$message({
-				type: type,
-				message: message,
-				duration: duration
-			})
-		},
+    errMessage(message = 'error', type = 'error', duration = 2000) {
+      this.$message({
+        type: type,
+        message: message,
+        duration: duration
+      })
+    },
 
-		/**
+    /**
          * 刷新当前路由
          */
-		reloadUrl() {
-			history.go(0)
-		}
-	}
+    reloadUrl() {
+      history.go(0)
+    }
+  }
 }
--- src/views/permission/log.vue
+++ src/views/permission/log.vue
@@ -1,34 +1,40 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-form size="medium">
+      <el-form>
         <el-form-item>
-          <el-button type="danger" @click="selectDelete" icon="el-icon-delete"
-            >删除</el-button
-          >
+          <el-button
+            type="danger"
+            icon="el-icon-delete"
+            @click="selectDelete"
+          >批量删除</el-button>
         </el-form-item>
       </el-form>
     </div>
-    <div class="content-container" v-loading="listLoading">
+    <div v-loading="listLoading" class="content-container">
       <el-table
-         :cell-style="{ textAlign: 'center' }"
         :data="list"
         border
         style="width: 100%"
         row-key="id"
         @selection-change="handleSelectionChange"
-        size="medium"
-        :header-cell-class-name="headerStyle"
       >
-        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column type="selection" width="55" />
         <el-table-column prop="name" label="name">
-
+          <template slot-scope="{ row }">
+            <el-tag>{{ row.name }}</el-tag>
+          </template>
         </el-table-column>
         <el-table-column prop="address" label="address">
+          <template slot-scope="{ row }">
+            {{ row.address }}
+          </template>
         </el-table-column>
 
         <el-table-column prop="url" label="路由">
-
+          <template slot-scope="{ row }">
+            <el-tag>{{ row.url }}</el-tag>
+          </template>
         </el-table-column>
 
         <el-table-column prop="method" label="方法">
@@ -37,16 +43,22 @@
           </template>
         </el-table-column>
         <el-table-column prop="ip" label="ip">
+          <template slot-scope="{ row }">
+            <el-tag>{{ row.ip }}</el-tag>
+          </template>
         </el-table-column>
         <el-table-column prop="created_at" label="时间">
+          <template slot-scope="{ row }">
+            <el-tag>{{ row.created_at }}</el-tag>
+          </template>
         </el-table-column>
-        <el-table-column label="操作" width="250px" fixed="right">
+        <el-table-column label="操作" width="250px">
           <template slot-scope="{ row }">
             <el-button
-              @click="del(row)"
               type="danger"
               icon="el-icon-delete"
-            ></el-button>
+              @click="del(row)"
+            />
           </template>
         </el-table-column>
       </el-table>
@@ -55,18 +67,18 @@
       v-show="total > 0"
       :total="total"
       :page.sync="listQuery.page"
-      :pageSize.sync="listQuery.pageSize"
+      :page-size.sync="listQuery.pageSize"
       @pagination="getList"
     />
   </div>
 </template>
 
 <script>
-import layoutMinix from "@/layout/components/mixins/layout";
-import { getLogList, delLog } from "@/api/auth";
-import Pagination from "@/components/Pagination/index";
+import layoutMinix from '../../layout/components/mixins/layout'
+import { getLogList, delLog } from '@/api/auth'
+import Pagination from '@/components/Pagination/index'
 export default {
-  name: "Log",
+  name: 'Log',
   components: { Pagination },
   mixins: [layoutMinix],
   data() {
@@ -79,86 +91,83 @@ export default {
       checkedPermission: undefined,
       multipleSelection: [],
       defaultProps: {
-        children: "children",
-        label: "name",
-        filter: [],
-      },
-    };
+        children: 'children',
+        label: 'name',
+        filter: []
+      }
+    }
+  },
+  mounted() {
+    this.getList()
   },
   methods: {
     handleClose(done) {
-      this.$confirm("确认关闭？")
+      this.$confirm('确认关闭？')
         .then((_) => {
-          done();
+          done()
         })
-        .catch((_) => {});
+        .catch((_) => {})
     },
     /**
      * 获取列表
      */
     getList(params = {}) {
-      params = Object.assign(params, this.filter);
+      params = Object.assign(params, this.filter)
       getLogList(params).then((response) => {
-        const { list, mate } = response.data;
-        this.list = list;
-        this.total = mate.total;
-        this.page = mate.page;
-        this.pageSize = mate.pageSize;
-      });
+        const { list, mate } = response.data
+        this.list = list
+        this.total = mate.total
+        this.page = mate.page
+        this.pageSize = mate.pageSize
+      })
     },
 
     /**
      * 删除权限节点
      */
-    headerStyle({ row, rowIndex }) {
-      return "table-th";
-    },
     async del(item) {
-      this.$confirm("此操作将永久删除权限, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
+      this.$confirm('此操作将永久删除权限, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
       })
         .then(() => {
           delLog(item.id).then((response) => {
             this.$notify({
-              type: "success",
-              message: "删除成功!",
-            });
-            this.getList();
-          });
+              type: 'success',
+              message: '删除成功!'
+            })
+            this.getList()
+          })
         })
         .catch(() => {
           this.$notify({
-            type: "info",
-            message: "已取消删除",
-          });
-        });
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
     },
     selectDelete() {
-      if (this.multipleSelection.length == 0) {
+      if (this.multipleSelection.length === 0) {
         this.$notify({
-          type: "warning",
-          message: "请选择需要删除的数据",
-        });
+          type: 'warning',
+          message: '请选择需要删除的数据'
+        })
       } else {
-        var ids = [];
+        var ids = []
         for (var i = 0; i < this.multipleSelection.length; i++) {
-          ids.push(this.multipleSelection[i]["id"]);
+          ids.push(this.multipleSelection[i]['id'])
         }
-        this.del({ id: ids.join(",") });
+        this.del({ id: ids.join(',') })
       }
     },
     handleSelectionChange(val) {
-      this.multipleSelection = val;
+      this.multipleSelection = val
     },
 
     handleRolesChange() {
-      this.$router.push({ path: "/permission/index?" + +new Date() });
-    },
-  },
-  mounted() {
-    this.getList();
-  },
-};
+      this.$router.push({ path: '/permission/index?' + +new Date() })
+    }
+  }
+}
 </script>
